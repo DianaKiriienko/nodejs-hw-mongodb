f0cebe129b727edca801e6b0200402d994a1c7b5
DianaKiriienko <diana.kirienko.homiak@gmail.com>
2024-06-12 21:15:09 +0300

    Summary

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import cors from 'cors';
 import pino from 'pino-http';
 
 import { env } from './utils/env.js';
-import { getAllContacts, getContactsById} from './db/services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -13,50 +15,7 @@ export const setupServer = () => {
     app.use(express.json());
     app.use(cors());
 
-    app.get('/contacts', async (req, res) => {
-        try {
-            const contacts = await getAllContacts();
-
-            res.status(200).json({
-                status: '200',
-                message: 'Successfully found contacts!',
-                data: contacts,
-            });
-        } catch (error) {
-            res.status(500).json({
-                status: '500',
-                message: 'Failed to retrieve contacts',
-                error: error.message,
-            });
-        }
-    });
-
-
-    app.get('/contacts/:contactId', async (req, res) => {
-        try {
-            const { contactId } = req.params;
-            const contact = await getContactsById(contactId);
-
-            if (!contact) {
-                return res.status(404).json({
-                status: '404',
-                message: 'Not found',
-                });
-            }
-
-            res.status(200).json({
-                status: '200',
-                message: `Successfully found contact with id ${contactId}!`,
-                data: contact,
-            });
-        } catch (error) {
-            res.status(404).json({
-                status: '404',
-                message: 'Failed to retrieve contacts',
-                error: error.message,
-            });
-        }
-    });
+    app.use(contactsRouter);
 
     app.use(
         pino({
@@ -66,18 +25,9 @@ export const setupServer = () => {
         }),
     );
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
+    app.use('*', notFoundHandler);
 
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
