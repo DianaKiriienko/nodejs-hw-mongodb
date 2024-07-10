402c6e112521177d1c6fe12de513f14350bccf90
DianaKiriienko <diana.kirienko.homiak@gmail.com>
2024-07-10 21:39:30 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import { UPLOAD_DIR } from './constans/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = env.PORT || 3000;
 
@@ -32,6 +33,9 @@ export const setupServer = () => {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use(notFoundHandler);
